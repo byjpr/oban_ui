f0baef53f176bf53ad2138aa209feea1179226c3
sushant12 <sussyoung9@gmail.com>
2020-09-07 08:08:11 +0545

    [WIP] implement url to view jobs in differnt state

--- lib/oban_ui/job_controller.ex
+++ lib/oban_ui/job_controller.ex
@@ -17,4 +17,52 @@ defmodule ObanUi.JobController do
       to: :"#{Application.get_env(:oban_ui, :app_name)}.Router.Helpers".job_path(conn, :show)
     )
   end
+
+  def available(conn, _params) do
+    {Ecto, repo} = Application.get_env(:oban_ui, :connection)
+
+    oban_jobs =
+      Ecto.Query.from(j in ObanUi.Job, where: j.state == "completed")
+      |> repo.all(ObanUi.Job)
+
+    render(conn, "show.html", oban_jobs: oban_jobs)
+  end
+
+  def scheduled(conn, _params) do
+    {Ecto, repo} = Application.get_env(:oban_ui, :connection)
+
+    oban_jobs = repo.all(ObanUi.Job)
+    render(conn, "show.html", oban_jobs: oban_jobs)
+  end
+
+  def executing(conn, _params) do
+    {Ecto, repo} = Application.get_env(:oban_ui, :connection)
+
+    oban_jobs = repo.all(ObanUi.Job)
+    render(conn, "show.html", oban_jobs: oban_jobs)
+  end
+
+  def retryable(conn, _params) do
+    {Ecto, repo} = Application.get_env(:oban_ui, :connection)
+
+    oban_jobs = repo.all(ObanUi.Job)
+    render(conn, "show.html", oban_jobs: oban_jobs)
+  end
+
+  def completed(conn, _params) do
+    {Ecto, repo} = Application.get_env(:oban_ui, :connection)
+
+    oban_jobs =
+      Ecto.Query.from(j in ObanUi.Job, where: j.state == "completed")
+      |> repo.all(ObanUi.Job)
+
+    render(conn, "show.html", oban_jobs: oban_jobs)
+  end
+
+  def discarded(conn, _params) do
+    {Ecto, repo} = Application.get_env(:oban_ui, :connection)
+
+    oban_jobs = repo.all(ObanUi.Job)
+    render(conn, "show.html", oban_jobs: oban_jobs)
+  end
 end
--- lib/oban_ui/router.ex
+++ lib/oban_ui/router.ex
@@ -3,6 +3,7 @@ defmodule ObanUi.Router do
     quote bind_quoted: binding() do
       scope path, alias: false, as: false do
         get("/", ObanUi.JobController, :show)
+        get("/completed", ObanUi.JobController, :completed)
         delete("/:job_id", ObanUi.JobController, :delete)
       end
     end
