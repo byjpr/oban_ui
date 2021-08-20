3cacc665f0e555002ac089eefbaf200e7b533f16
Jordan Parker <jgit@fmail.co.uk>
2021-08-20 19:04:46 +0100

    Add pill state classes Switches styles of pills based on their value

--- lib/oban_ui/job_view.ex
+++ lib/oban_ui/job_view.ex
@@ -1,3 +1,16 @@
 defmodule ObanUi.JobView do
   use ObanUi.Web, :view
+
+  @spec state_classes(<<_::72>>) :: <<_::184, _::_*32>>
+  def state_classes(state) do
+    case state do
+      "available" -> "text-green-700 bg-green-100"
+      "scheduled" -> "text-orange-700 bg-gray-100"
+      "executing" -> "text-orange-700 bg-gray-100"
+      "retryable" -> "text-orange-700 bg-gray-100"
+      "completed" -> "text-green-700 bg-green-100"
+      "cancelled" -> "text-red-700 bg-red-100"
+      "discarded" -> "text-red-700 bg-red-100"
+    end
+  end
 end
